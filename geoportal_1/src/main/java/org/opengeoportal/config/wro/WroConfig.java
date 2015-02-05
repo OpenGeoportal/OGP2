a073c957eb5abe42be9b4cb154544bb41013e8b4
Christopher Barnett <chrissbarnett@gmail.com>
2015-02-05 13:00:38 -0500

    added new module to read javascript and css resources directly from wro.xml to be included when '?dev=true'.
    this means devincludes.jsp is no longer needed and resources only have
    to be enumerated in one place.

---
+++ geoportal_1/src/main/java/org/opengeoportal/config/wro/WroConfig.java
@@ -0,0 +1,22 @@
+package org.opengeoportal.config.wro;
+
+public class WroConfig {
+
+	String[] css;
+	String[] js;
+	
+	public String[] getCss() {
+		return css;
+	}
+	public void setCss(String[] css) {
+		this.css = css;
+	}
+	public String[] getJs() {
+		return js;
+	}
+	public void setJs(String[] js) {
+		this.js = js;
+	}
+	
+
+}
