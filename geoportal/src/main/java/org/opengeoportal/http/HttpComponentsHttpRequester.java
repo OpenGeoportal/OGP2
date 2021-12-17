024524cebd5c526bfacd9ed8d03073f265fa78c0
chrissbarnett <chrissbarnett@gmail.com>
2021-12-17 17:16:22 -0500

    WIP: not ready to run initial commit native HttpClient based HttpRequester removed unused jsonp filter

--- geoportal/src/main/java/org/opengeoportal/http/HttpComponentsHttpRequester.java
+++ geoportal/src/main/java/org/opengeoportal/http/HttpComponentsHttpRequester.java
@@ -22,8 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
-@Scope("prototype")
+
 public class HttpComponentsHttpRequester implements HttpRequester {
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
